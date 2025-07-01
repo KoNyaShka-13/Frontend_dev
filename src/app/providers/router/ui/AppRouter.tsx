1dab80b9919dc8bb080bfcee7b6cb4bbb7e26df1
Alexei <97723655+KoNyaShka-13@users.noreply.github.com>
2025-07-01 17:51:11 +0300

    Loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,25 +1,24 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 // Определяем роуты на уровне конфига
 const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({ element, path }) => (// Делаем так, чтобы получить массив нужных данных
-                <Route
-                    key={path}
-                    path={path}
-                    element={(
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <div className="page-wrapper">
-                                {element}
-                            </div>
-                        </Suspense>// Два раза в suspense не должно быть обернуто, если что. Так не правильно
-                    )}// Так выглядит структура импортируемого компонента
-                />
-            ))}
-        </Routes>
-    </Suspense>
+    <Routes>
+        {Object.values(routeConfig).map(({ element, path }) => (// Делаем так, чтобы получить массив нужных данных
+            <Route
+                key={path}
+                path={path}
+                element={(
+                    <Suspense fallback={<PageLoader />}>
+                        <div className="page-wrapper">
+                            {element}
+                        </div>
+                    </Suspense>// Два раза в suspense не должно быть обернуто, если что. Так не правильно
+                )}// Так выглядит структура импортируемого компонента
+            />
+        ))}
+    </Routes>
 );
 
 export default AppRouter;
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,19 @@
 import { RouteProps } from 'react-router-dom';
 import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {// Обозначаем
     MAIN = 'main',
     ABOUT = 'about',
+    NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = { // Их пути
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
+    // Последним должен быть, а *, так как охватывает все файлы
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = { // Объявляем сами роуты
@@ -20,5 +24,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = { // Объявляе�
     [AppRoutes.ABOUT]: {
         path: RoutePath.about,
         element: <AboutPage />,
+    },
+    [AppRoutes.NOT_FOUND]: {
+        path: RoutePath.not_found,
+        element: <NotFoundPage />,
     },
 };
