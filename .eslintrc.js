6c79543a81c048f94462d74308b8dfbf98d4c074
KoNyaShKa <ggiziawpgg@gmail.com>
2025-07-17 15:13:16 +0300

    settings tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,10 @@ module.exports = {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'warn', // Предупреждения вместо ошибок для неиспользуемых переменных
@@ -46,4 +49,12 @@ module.exports = {
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
